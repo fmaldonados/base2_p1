458d4001d2a47a7752328ac5177e8aad1de56eb9
Alexy Cruz <xytojr@gmail.com>
2017-05-27 21:37:58 -0600

    borrando btn-group

--- FrontEnd/src/components/About.vue
+++ FrontEnd/src/components/About.vue
@@ -3,18 +3,6 @@
     <h1 class="page-header">About</h1>
     <p>Simple app to create questions for your students</p>    
     <p>Version 17.01</p>  
-
-<body id="app">
-  <div class="container">
-      <div class="btn-group" data-toggle="buttons" v-radio="value">
-         <label class="btn btn-primary"> <input type="radio" autocomplete="off" value="R1" />Radio 1</label>
-         <label class="btn btn-primary"> <input type="radio" autocomplete="off" value="R2" />Radio 2</label>
-         <label class="btn btn-primary"> <input type="radio" autocomplete="off" value="R3" />Radio 3</label>
-      </div>
-     <h3> Result: {{value}} </h3>
-  </div>
-</body>
-    
   </div>
 </template>
 
